eea9c936e78f8b33212c8a48c6775f8d1ce48eca
Brooke Kuhlmann <brooke@alchemists.io>
2019-09-19 19:59:41 -0600

    Added pair presence checks.
    Provides convenience methods for checking on the presence of the pair.

--- lib/xdg/pair.rb
+++ lib/xdg/pair.rb
@@ -6,5 +6,17 @@ module XDG
     def to_env
       Hash[*values]
     end
+
+    def key?
+      key.to_s.size.positive?
+    end
+
+    def value?
+      value.to_s.size.positive?
+    end
+
+    def empty?
+      !(key? && value?)
+    end
   end
 end
--- spec/lib/xdg/pair_spec.rb
+++ spec/lib/xdg/pair_spec.rb
@@ -3,11 +3,21 @@
 require "spec_helper"
 
 RSpec.describe XDG::Pair do
-  subject(:pair) { described_class.new "TEST", "example" }
+  subject(:pair) { described_class["TEST", "example"] }
 
   describe "#key" do
     it "answers key" do
       expect(pair.key).to eq("TEST")
+    end
+  end
+
+  describe "#key?" do
+    it "answers true when key is present" do
+      expect(described_class["TEST"].key?).to eq(true)
+    end
+
+    it "answers false when key is missing" do
+      expect(described_class.new.key?).to eq(false)
     end
   end
 
@@ -17,9 +27,37 @@ RSpec.describe XDG::Pair do
     end
   end
 
+  describe "#value?" do
+    it "answers true when value is present" do
+      expect(described_class["TEST", "example"].value?).to eq(true)
+    end
+
+    it "answers false when value is missing" do
+      expect(described_class["TEST"].value?).to eq(false)
+    end
+  end
+
   describe "#to_env" do
     it "answers environment" do
       expect(pair.to_env).to eq("TEST" => "example")
+    end
+  end
+
+  describe "#empty?" do
+    it "answers false when key and value are present" do
+      expect(described_class["TEST", "example"].empty?).to eq(false)
+    end
+
+    it "answers true with key only" do
+      expect(described_class["TEST"].empty?).to eq(true)
+    end
+
+    it "answers true with value only" do
+      expect(described_class[nil, "example"].empty?).to eq(true)
+    end
+
+    it "answers true when both key and value are missing" do
+      expect(described_class[].empty?).to eq(true)
     end
   end
 end
