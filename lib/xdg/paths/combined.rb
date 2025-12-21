e3301641e38e1311aa135c1cd258c570b01feca8
Brooke Kuhlmann <brooke@alchemists.io>
2025-12-21 14:52:52 -0700

    Updated to Ruby 4.0.0
    link:https://www.ruby-lang.org/en/news/2025/12/25/ruby-4-0-0-released[Details]
    
    Milestone: major

--- lib/xdg/paths/combined.rb
+++ lib/xdg/paths/combined.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "pathname"
-
 module XDG
   module Paths
     # The combined home and directory paths.
--- lib/xdg/paths/directory.rb
+++ lib/xdg/paths/directory.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "pathname"
-
 module XDG
   module Paths
     # A collection of XDG directories.
